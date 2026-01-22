199d5eb95e0241698faba1be4938f89e5e47d883
implement-test <reducewastewithideas@gmail.com>
2026-01-22 15:47:25 +0900

    3D 뷰어 기능 1차 업로드

---
+++ config.js
@@ -0,0 +1,6 @@
+// config.js
+window.SupabaseConfig = {
+    url: "https://dcerakixrvvjftokntqe.supabase.co", // 본인의 Supabase URL
+    key: "sb_publishable_xwHnw6lYI6CyLEhD0hiAiQ_D93LnqZ6",                       // 본인의 Supabase Anon Key
+    bucket: "models"                            // 생성한 버킷 이름
+};
